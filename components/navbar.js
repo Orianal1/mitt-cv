dd8dee5400d6425d287c90d88fe041c9a601fd5e
Orianal1 <orianalama@fredriks-mbp.lan>
2026-01-09 21:50:15 +0100

    Testar att uppdatera flera filer

--- components/navbar.js
+++ components/navbar.js
@@ -1,11 +1,12 @@
 const NavBar = {
 	template: `
     <nav>
-		<div class="logo">Oriana Lama</div>
+		<div class="logo">Oriana Lama är bäst!</div>
 		<ul class="nav-links">
 			<li><a href="index.html">Hem</a></li>
 			<li><a href="about.html">Om</a></li>
 			<li><a href="contact.html">Kontakt</a></li>
+			<li><a href="finns-inte.html>Sidan under uppbyggnad</a></li>
 		</ul>
 	</nav>
   `,
