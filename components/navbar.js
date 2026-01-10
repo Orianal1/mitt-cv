82355219ef7404ec31c8cee304a53e870856ce4c
Orianal1 <orianalama@fredriks-mbp.lan>
2026-01-10 14:25:07 +0100

    hmm

--- components/navbar.js
+++ components/navbar.js
@@ -4,9 +4,8 @@ const NavBar = {
 		<div class="logo">Oriana Lama</div>
 		<ul class="nav-links">
 			<li><a href="index.html">Hem</a></li>
-			<li><a href="about.html">Om</a></li>
+			<li><a href="about.html">Om Mig</a></li>
 			<li><a href="contact.html">Kontakt</a></li>
-			<li><a href="finns-inte.html>Sidan under uppbyggnad</a></li>
 		</ul>
 	</nav>
   `,
