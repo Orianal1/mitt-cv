9ba6c9e2fd5972284bb89cc27e80e46d580089f1
Orianal1 <orianalama@fredriks-mbp.lan>
2026-01-11 17:45:44 +0100

    validation

--- script.js
+++ script.js
@@ -12,13 +12,50 @@ document.addEventListener("DOMContentLoaded", () => {
 	// Simple Form Handler Log
 	const contactForm = document.getElementById("contact-form");
 	if (contactForm) {
-		contactForm.addEventListener("submit", (e) => {
+		contactForm.addEventListener("submit", (event) => {
+			event.preventDefault();
+		
+		
+			const name = document.getElementById("name").value;
+			const email = document.getElementById("email").value;
+			const message = document.getElementById("message").value;
+		
+			console.log("Form submitted:");
+			console.log("Name:", name);
+			console.log("Email:", email);
+			console.log("Message:", message);
+		
+			if (!name || !email || !message) {
+				alert("Vänligen fyll i alla fält innan du skickar meddelandet.");
+				return;
+			}
+		
+			if (name.length < 2) {
+				const nameError = document.getElementById("name-error");
+				<p class="error-message" id="name-error"></p>
+				nameError.textContent = "Namnet måste vara minst 2 tecken långt.";
+				<p class="error-message" id="name-error">Namnet måste vara ....</p>
+				nameError.style.display = "block";
+				return;
+			}
+			if (email.length < 5 || !email.includes("@")) {
+				const emailError = document.getElementById("email-error");
+				emailError.textContent = "E-postadressen måste vara minst 5 tecken lång.";
+				emailError.style.display = "block";
+				return;
+			}
+		
+			alert(
+				`Tack för ditt meddelande, ${name}! Jag återkommer till dig på ${email} så snart som möjligt.`
+			);
+		});
+		/*contactForm.addEventListener("submit", (e) => {
 			// Prevent actual submission as requested
 			// Standard form submission is returned false in HTML, but we preventDefault here too just in case
 			e.preventDefault();
 			console.log("Form submitted (simulation)");
 			alert("Tack för ditt meddelande! (Detta är bara en demo)");
 			contactForm.reset();
-		});
+		});*/
 	}
 });
