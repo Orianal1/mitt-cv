43fc4af80450edb8c2104dd38d1f65b1880f8e58
Orianal1 <orianalama@fredriks-mbp.lan>
2026-01-12 10:13:49 +0100

    fonts

--- script.js
+++ script.js
@@ -16,17 +16,6 @@ document.addEventListener("DOMContentLoaded", () => {
 			e.preventDefault();
 			const name = document.getElementById("name").value;
 			const email = document.getElementById("email").value;
-			const message = document.getElementById("message").value;
-
-			console.log("Form submitted:");
-			console.log("Name:", name);
-			console.log("Email:", email);
-			console.log("Message:", message);
-
-			if (!name || !email || !message) {
-				alert("Vänligen fyll i alla fält innan du skickar meddelandet.");
-				return;
-			}
 
 			if (name.length < 2) {
 				const nameError = document.getElementById("name-error");
@@ -34,6 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
 				nameError.style.display = "block";
 				return;
 			}
+
 
 			alert(
 				`Tack för ditt meddelande, ${name}! Jag återkommer till dig på ${email} så snart som möjligt.`
